6755d65402d0804085895dc0f9a100cb777bd083
Faizan Aalam <f24aalam@gmail.com>
2022-10-07 09:22:52 +0000

    Added CRUD functions

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def createStudent():
+  print("create student")
+
+
+def updateStudent():
+  print("update student")
+
+
+def deleteStudent():
+  print("delete student")
+
+
+def listAllStudent():
+  print("list all student")
+
+
 print("Operations")
 print("1. Create a student")
 print("2. Update a student")
