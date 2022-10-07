a3d544ea12307d29f5c85454babc5802d0da857b
Faizan Aalam <f24aalam@gmail.com>
2022-10-07 09:37:51 +0000

    Updated now

--- main.py
+++ main.py
@@ -14,10 +14,27 @@ def listAllStudent():
   print("list all student")
 
 
-print("Operations")
-print("1. Create a student")
-print("2. Update a student")
-print("3. Delete a student")
-print("4. List all students")
-print("\n-------------------------\n")
-choice = int(input("Input your choice: "))
+choice = 0
+
+while choice != 5:
+  print("Operations")
+  print("1. Create a student")
+  print("2. Update a student")
+  print("3. Delete a student")
+  print("4. List all students")
+  print("5. Exit")
+  print("\n-------------------------\n")
+  choice = int(input("Input your choice: "))
+
+  if choice == 1:
+    createStudent()
+  elif choice == 2:
+    updateStudent()
+  elif choice == 3:
+    deleteStudent()
+  elif choice == 4:
+    listAllStudent()
+  elif choice == 5:
+    print("Bye")
+  else:
+    print("Wrong choice")
